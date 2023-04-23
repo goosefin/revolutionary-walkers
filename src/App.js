230163770d2151bdd755b5aec9ac529038d5d7e5
goosefin <agustinafin96@gmail.com>
2023-04-23 12:40:30 -0400

    landing page styled

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
+    <div id="landing-main">
       <NavBar/>
-      <h2>Body</h2>
-    </>
+
+      <div className='flex flex-col items-center justify-center' id="slogan">
+        <p className="slogan-text">FOR YOU, THEY'RE MORE THAN JUST A PET</p>
+        <p className="slogan-text">FOR US, IT'S MORE THAN JUST A JOB</p>
+      </div>
+    </div>
     
   );
 }
