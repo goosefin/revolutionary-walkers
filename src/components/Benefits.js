6cc1ab6423885df4613b9fd2847116b37b372417
goosefin <agustinafin96@gmail.com>
2023-04-25 19:29:46 -0400

    Benefits component styled

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -2,11 +2,13 @@
 
 function Benefits() {
     return(
-        <div className="flex flex-col bg-gradient-to-r from-cyan-400 to-sky-500" id="benefits-div">
-            <h2 id="benefits-h2">WHAT ARE THE BENEFITS OF HIRING A DOG WALKER?</h2>
-            <p>Often times, busy NYC dog owners find themselves asking the million-dollar question— ”do I need a dog walker?” Between juggling early mornings and late nights at the office, weekend brunch with friends, and training for that marathon, finding the time to ensure that your pet is being exercised and socialized properly can be tricky. This is where hiring a reliable dog walker comes in handy.</p>
-            <p>Although some breeds may need more exercise than others, it’s important to note that a dog in overall good health should be getting at least 30-60 minutes of physical activity every day. Apart from physical health, socialization is another great reason to consider hiring a walker. Canine socialization is important even long after your furry friend is no longer a puppy, you should continuously be exposing your pet to new dogs, people, places, sounds, and so on no matter how old they are. </p>
-            <p>From relaxing strolls by the river to fun romps at the dog park and everything in between, hiring a dog walker can give you peace of mind in knowing that your pet is well taken care of, even when you’re not home. After all, a tired dog is a happy dog!</p>
+        <div className="bg-gradient-to-r from-cyan-400 to-sky-500" id="benefits-div">
+            <div className="flex flex-col justify-between items-center" id="benefits-text-div">
+                <h2 className="text-4xl" id="benefits-h2">WHAT ARE THE BENEFITS OF HIRING A DOG WALKER?</h2>
+                <p className="text-white text-xl font-semibold">Often times, busy NYC dog owners find themselves asking the million-dollar question— ”do I need a dog walker?” Between juggling early mornings and late nights at the office, weekend brunch with friends, and training for that marathon, finding the time to ensure that your pet is being exercised and socialized properly can be tricky. This is where hiring a reliable dog walker comes in handy.</p>
+                <p className="text-white text-xl font-semibold">Although some breeds may need more exercise than others, it’s important to note that a dog in overall good health should be getting at least 30-60 minutes of physical activity every day. Apart from physical health, socialization is another great reason to consider hiring a walker. Canine socialization is important even long after your furry friend is no longer a puppy, you should continuously be exposing your pet to new dogs, people, places, sounds, and so on no matter how old they are. </p>
+                <p className="text-white text-xl font-semibold">From relaxing strolls by the river to fun romps at the dog park and everything in between, hiring a dog walker can give you peace of mind in knowing that your pet is well taken care of, even when you’re not home. After all, a tired dog is a happy dog!</p>
+            </div>
         </div>
     )
 }
